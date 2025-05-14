bff9c065abe5b8f54888e2926fc0aaf2c791356e
doonska <727187@student.dusd.net>
2025-05-14 11:45:37 -0700

    U5
    Got to step 3 on 5.2

---
+++ U5ClickyCratesP4Villalobos/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    private float spawnrate = 1.0f;
+    private int score;
+    public TextMeshProUGUI scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        scoreText.text = "Score: " + score;
+    }
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnrate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
