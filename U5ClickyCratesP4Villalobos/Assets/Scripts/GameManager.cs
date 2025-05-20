ee88e21c5c64d064ed2b2096076e6e373292876e
doonska <727187@student.dusd.net>
2025-05-20 11:44:50 -0700

    U5
    finished main. need to do last 2 bonus steps

--- U5ClickyCratesP4Villalobos/Assets/Scripts/GameManager.cs
+++ U5ClickyCratesP4Villalobos/Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,21 +12,39 @@ public class GameManager : MonoBehaviour
     private int score;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
+    public Button restartButton;
+    public GameObject titleScreen;
+    public TextMeshProUGUI livesText;
+    private int lives;
     // Start is called before the first frame update
-    void Start()
+   public void StartGame(int difficulty)
     {
         StartCoroutine(SpawnTarget());
         score = 0;
         UpdateScore(0);
+        isGameActive = true;
+        titleScreen.gameObject.SetActive(false);
+        spawnrate /= difficulty;
+        UpdateLives(3);
+        titleScreen.gameObject.SetActive(false);
+    }
+
+    
+    void Start()
+    {
+        
     }
 
     public void GameOver()
     {
         gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+        restartButton.gameObject.SetActive(true);
     }
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnrate);
             int index = Random.Range(0, targets.Count);
@@ -38,10 +58,25 @@ public class GameManager : MonoBehaviour
         scoreText.text = "Score: " + score;
     }
 
+    public void UpdateLives(int livesToChange)
+    {
+        lives += livesToChange;
+        livesText.text = "Lives: " + lives;
+        if (lives <= 0)
+        {
+            GameOver();
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
